5d191138e200d96059d22a4dc2326a3ec157f5de
ryandonoghue20 <ryandonoghue06@icloud.com>
2025-10-25 12:33:44 -0400

    uncommenting first free in remove

--- symtablelist.c
+++ symtablelist.c
@@ -200,8 +200,7 @@ void *SymTable_remove(SymTable_T oSymTable, const char *pcKey)
             if (psCurrentNode == oSymTable->psFirstNode) 
             {
                 pvRemovedValue = oSymTable->psFirstNode->pvValue; 
-                psCurrentNode->psNextNode = oSymTable->psFirstNode->psNextNode; 
-                /* free(oSymTable->psFirstNode->pcKey); */
+                free(oSymTable->psFirstNode->pcKey);
                 /* free(oSymTable->psFirstNode); */
                 oSymTable->psFirstNode = psCurrentNode->psNextNode;
                 oSymTable->uLength--; 
